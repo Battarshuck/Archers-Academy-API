069461fbfba3a18381be5cf990df24a12f480e56
Mostafa Ossama <ossamamostafa5@gmail.com>
2018-04-07 11:15:49 +0200

    project dependencies creation

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose.connect('mongodb://localhost/archers-academy');
+
+
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extneded: false}));
